3cf2383ae6c7608a9a538e1c37a5420e2f7e0191
dltslt <87982398+dltslt@users.noreply.github.com>
2026-05-17 23:09:17 +0200

    refactor: stack use std::list

--- ex01/inc/RPN.hpp
+++ ex01/inc/RPN.hpp
@@ -6,13 +6,14 @@
 /*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 09:44:41 by mweghofe          #+#    #+#             */
-/*   Updated: 2026/05/13 11:45:22 by mweghofe         ###   ########.fr       */
+/*   Updated: 2026/05/17 23:08:39 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <stack>
+#include <list>
 
 #ifndef DEBUG
 # define DEBUG 0
@@ -22,7 +23,7 @@ class RPN
 {
 	private:
 	// ---
-	std::stack<int> stack_;
+	std::stack<int, std::list<int> > stack_;
 	void operation(char& c);
 	public:
 	// ---
